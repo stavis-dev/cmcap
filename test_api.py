a4c0bf41314676992d51ddaab51a932e40720917
Stavis <stavis.vega@gmail.com>
2022-05-20 10:23:46 +0300

    coment to test

--- test_api.py
+++ test_api.py
@@ -11,7 +11,9 @@ class TestApi3(unittest.TestCase):
 
         self.assertEqual(type(result), dict)
         self.assertEqual("0", result["status"]["error_code"])
+        # check tables of excanges
         self.assertEqual(type(result["data"]["exchangeMap"]), list)
+        # check tables of crypto Currency
         self.assertEqual(type(result["data"]["cryptoCurrencyMap"]), list)
 
 
