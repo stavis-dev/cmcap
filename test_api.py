23ee85fbe392300aab32df8f913bba3cf3ccbc90
Stavis <stavis.vega@gmail.com>
2022-06-14 04:47:42 +0300

    add test for save data for mock

--- test_api.py
+++ test_api.py
@@ -20,29 +20,29 @@ class TestApiwithMock(unittest.TestCase):
         print(f"\n {'-' * 20} \n result = {result}")
 
 
-@unittest.skip("skipping real connect test")
+# @unittest.skip("skipping real connect test")
 class TestPriceConversion(unittest.TestCase):
 
     # @unittest.skip("skipping price_conversion")
     def test_price_conversion_ok(self):
         amount = 1
         id: int = 1  # BTC
-        convert_id: int = 2781  # USD
+        convert_id: int = 825  # USDT
 
 
         expected_symbol = "BTC"
-        expected_convert_symbol = "USD"
+        expected_convert_symbol = "USDT"
         expected_cerror_code = "0"
 
         result = api.price_conversion(amount, id, convert_id)
 
-        # print(result)
+        print(f"\n {'-' * 20} \n result = {result}")
 
         self.assertEqual(expected_cerror_code, result["status"]["error_code"])
         self.assertEqual(expected_symbol, result["data"]["symbol"])
         self.assertEqual(expected_convert_symbol, result["data"]["quote"][0]["symbol"])
 
-    # @unittest.skip("skipping price_conversion")
+    @unittest.skip("skipping price_conversion")
     def test_price_conversion_wrong_id(self):
         amount = 1
         id: int = 99999  # BTC
@@ -52,7 +52,7 @@ class TestPriceConversion(unittest.TestCase):
         result = api.price_conversion(amount, id, convert_id)
         self.assertEqual(expected_cerror_code, result["status"]["error_code"])
 
-    # @unittest.skip("skipping price_conversion wrong_convert_id")
+    @unittest.skip("skipping price_conversion wrong_convert_id")
     def test_price_conversion_wrong_convert_id(self):
         amount = 1
         id: int = 1  # BTC
@@ -70,7 +70,7 @@ class TestPriceConversion(unittest.TestCase):
         self.assertEqual(expected_cerror_code, result["status"]["error_code"])
         self.assertEqual(expected_len_quote_list, len_qute_list)
 
-    # @unittest.skip("skipping wrong_amount price_conversion ")
+    @unittest.skip("skipping wrong_amount price_conversion ")
     def test_price_conversion_wrong_amount(self):
         amount = -1
         id = 1  # BTC
---
+++ test_save_test_data.py
@@ -0,0 +1,47 @@
+import unittest
+from unittest.mock import patch, mock_open
+from cmcap import api
+import json
+
+
+# @unittest.skip("skipping macking local data")
+class TestMakeLocalJsonData(unittest.TestCase):
+
+    @unittest.skip("skipping price_conversion")
+    def test_save_price_conversion(self):
+        amount = 1
+        id: int = 1  # BTC
+        convert_id: int = 825  # USDT
+
+        params: dict = {"amount": amount,
+                        "id": id,
+                        "convert_id": convert_id}
+        result = api._get_request('/tools/price-conversion',
+                                  params)
+
+        with open('test_data/price_conversion.json', 'wb') as file:
+            file.write(result)
+
+        with open('test_data/price_conversion.json', 'rb') as file:
+            read = file.read()
+
+        # print(f"\n {'-' * 20} \n result = {result}")
+
+        self.assertEqual(read, result)
+
+
+    @unittest.skip("skipping map_all.json")
+    def test_save_map_all(self):
+        result = api._get_request('/map/all', None)
+        with open('test_data/map_all.json', 'wb') as file:
+            file.write(result)
+
+        with open('test_data/map_all.json', 'rb') as file:
+            read = file.read()
+
+        # print(f"\n {'-' * 20} \n result = {result}")
+
+        self.assertEqual(read, result)
+
+
+unittest.main()
