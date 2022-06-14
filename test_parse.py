84e4e04165aa5c363024ed368ee41ed6fc6e37ce
Stavis <stavis.vega@gmail.com>
2022-06-14 05:42:26 +0300

    add test for parse

---
+++ test_parse.py
@@ -0,0 +1,24 @@
+import unittest
+from unittest.mock import patch
+import json
+
+from cmcap.parse import price_conversion_parse, Conversion
+
+class TestParseConverter_mock(unittest.TestCase):
+
+    def test_price_conversion_parse(self):
+        with open('test_data/price_conversion.json', "r") as file:
+            cmcap_response = json.load(file)
+
+        # print(f"\n {'-' * 20} \n result = {cmcap_response}")
+
+        result = price_conversion_parse(cmcap_response)
+
+        print(f"\n {'-' * 20} \n {result}")
+
+
+
+
+
+unittest.main()
+
